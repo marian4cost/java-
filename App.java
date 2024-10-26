2797cb29331badcf5c9ff5199cc37ab2a7b574a3
marian4.cost4 <mariana.costa.lisboa5579@gmail.com>
2024-10-26 19:42:41 -0300

    tipos não primitivos 04

--- App.java
+++ App.java
@@ -1,9 +1,15 @@
 public class App{
     public static void main(String[] args) {
+        //TIPOS PRIMITIVOS
         int idade = 18; // 2147483647 esse é o valor limite que o int recebe, passou disso tem que ser long
         double altura = 1.48;
         char letra = 'a';
         long inteiroGigante = 2147483648L; 
+
+        //MEMÓRIA
         // sobre memória: int = 32 bits, double = 16 bits, char = 16 bits, long = 64 bits  
+
+        //TIPOS NÃO PRIMITIVOS
+        String texto = "mariana costa";
     }
 }
